cf325026abb20e71a8b4f8886fc130fd7ac3f9b4
Stephen <51303308+thaengineer@users.noreply.github.com>
2024-04-04 15:23:09 -0400

    Create AclOwner.ps1

---
+++ AclOwner.ps1
@@ -0,0 +1,21 @@
+Param (
+    [string]$Path = ''
+)
+
+if (-not (Test-Path -Path $Path)) {
+    return
+}
+
+$DomainGroup = '<domain>\<group>'
+$Account = New-Object -TypeName System.Security.Principal.NTAccount -ArgumentList $DomainGroup
+
+$Items = Get-ChildItem -Path "$($Path)"
+
+$Items | Foreach-Object {
+    $Acl = $null
+    $Acl = Get-Acl -Path $_.FullName
+    $Acl.SetOwner($Account)
+    $AccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule($Account, 'FullControl', 'ContainerInherit,ObjectInherit', 'None', 'Allow')
+    $Acl.SetAccessRule($AccessRule)
+    Set-Acl -Path $_.FullName -AclObject $Acl
+}
