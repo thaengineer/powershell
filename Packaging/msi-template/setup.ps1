0c2879acfae4f0733757abf07e9bc9541ec60b69
Stephen <51303308+thaengineer@users.noreply.github.com>
2025-02-12 21:49:14 -0500

    Update setup.ps1

--- Packaging/msi-template/setup.ps1
+++ Packaging/msi-template/setup.ps1
@@ -8,25 +8,32 @@ $Msi = Get-ChildItem -Filter "*.msi" | Select-Object -First 1
 
 Function Install-Application {
     Param (
+        [Parameter(Mandatory = $true, Position = 0)]
         [object]$Msi
     )
 
-    $ArgList = "/i `"$($Msi.Name)`" /qn /norestart"
+    $Version = ($Msi.Name | Select-String -Pattern "\d{2}(\.\d){1,2}").Matches.Value
+    $LogFile = "C:\Temp\Install-ProductName-$($Version).log"
+    $MsiArgs = "/i `"$($Msi.Name)`" /qn /norestart /l*v $($LogFile)"
 
     Start-Process -FilePath "msiexec.exe" -ArgumentList "$($ArgList)" -NoNewWindow -Wait -ErrorAction SilentlyContinue
 }
 
 Function Uninstall-Application {
-    Param (
-        [object]$Msi
-    )
+    $Products = [ordered]@{
+        "ProductName1-Version1" = "{00000000-0000-0000-0000-000000000000}"
+        "ProductName2-Version2" = "{00000000-0000-0000-0000-000000000000}"
+    }
 
-    $ArgList = "/x `"$($Msi.Name)`" /qn /norestart"
+    $Products.GetEnumerator() | Foreach-Object {
+        $LogFile = "C:\Temp\Uninstall-$().log"
+        $MsiArgs = "/x $($_.Value) /qn /norestart /l*v $($LogFile)"
 
-    Start-Process -FilePath "msiexec.exe" -ArgumentList "$($ArgList)" -NoNewWindow -Wait -ErrorAction SilentlyContinue
+        Start-Process -FilePath "msiexec.exe" -ArgumentList $MsiArgs -NoNewWindow -Wait -ErrorAction SilentlyContinue
+    }
 }
 
 Switch ($Action) {
     'Install'   { Install-Application -Msi $Msi }
-    'Uninstall' { Uninstall-Application -Msi $Msi }
+    'Uninstall' { Uninstall-Application }
 }
