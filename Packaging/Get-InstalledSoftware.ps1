8d15e86ec03930c88f5bcf19cba644ee1ce8e6cb
Stephen <51303308+thaengineer@users.noreply.github.com>
2025-02-12 22:02:36 -0500

    Update Get-InstalledSoftware.ps1

--- Packaging/Get-InstalledSoftware.ps1
+++ Packaging/Get-InstalledSoftware.ps1
@@ -1,5 +1,4 @@
-Param
-(
+Param (
     [parameter(mandatory=$True)]
     [string]$ComputerName,
 
@@ -7,36 +6,29 @@ Param
     [string]$Software
 )
 
+$Keys = @(
+    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+)
 
-$X86       = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-$X64       = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-$codeBlock = {
-    $Properties = Get-ChildItem -Path $X86, $X64 | Get-ItemProperty | Select-Object -Property DisplayName, UninstallString
-    $SortedList = $Properties | Sort-Object -Unique -Property DisplayName
-    #$UserName   = (Get-CimInstance -ClassName Win32_ComputerSystem).UserName.Split("\")[1]
-    #$SID        = (Get-ADUser -Identity $UserName).SID.Value
-    return $SortedList
+$ScriptBlock = {
+    $Products = Get-ItemProperty -Path $Keys -ErrorAction SilentlyContinue
+
+    return $Products
 }
 
-
-if(! (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet)) {
+if (-not (Test-Connection -ComputerName $ComputerName -Count 1 -Quiet)) {
     Write-Host -ForegroundColor Red "${ComputerName}: not reachable"
 } else {
-    if($Software -ne "") {
-        try {
-            Write-Host -ForegroundColor Green "${ComputerName}: searching for ${Software}"
-            $SWList = Invoke-Command -ComputerName $ComputerName -ScriptBlock $codeBlock
-            $SWList | Where-Object { $_.DisplayName -match $Software }
-        } catch {
-            Write-Host -ForegroundColor Red "${ComputerName}: unable to establish PSSession"
-        }
+    try {
+        $Products = Invoke-Command -ComputerName $ComputerName -ScriptBlock $ScriptBlock -ErrorAction Stop
+    } catch {
+        Write-Host -ForegroundColor Red "${ComputerName}: unable to establish PSSession"
+    }
+
+    if ($Product -ne "") {
+        $Products | Where-Object { $_.DisplayName -match $Product }
     } else {
-        try {
-            Write-Host -ForegroundColor Green "${ComputerName}: compiling full software list"
-            $SWList = Invoke-Command -ComputerName $ComputerName -ScriptBlock $codeBlock
-            $SWList
-        } catch {
-            Write-Host -ForegroundColor Red "${ComputerName}: unable to establish PSSession"
-        }
+        $Products
     }
 }
