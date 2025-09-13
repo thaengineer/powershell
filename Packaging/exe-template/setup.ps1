b83897cfb92fd59754f3fdb357f28a975e6bd37e
Stephen <51303308+thaengineer@users.noreply.github.com>
2025-09-13 19:15:21 -0400

    Update setup.ps1

--- Packaging/exe-template/setup.ps1
+++ Packaging/exe-template/setup.ps1
@@ -1,30 +1,29 @@
-Param (
-    [ValidateSet("Install", "Uninstall", IgnoreCase = $true)]
+param (
+    [ValidateSet('Install', 'Uninstall', IgnoreCase = $true)]
     [Parameter(Mandatory = $false, Position = 0)]
-    [string]$Action = "Install"
+    [string]$Action = 'Install'
 )
 
-$Exe = Get-ChildItem -Filter "*.exe" | Select-Object -First 1
 
-Function Install-Application {
-    Param (
-        [Parameter(Mandatory = $true, Position = 0)]
-        [object]$Exe
-    )
+function Install-Application {
+    $Exe     = Get-ChildItem -Filter '*.exe' | Select-Object -First 1
+    $ExeArgs = ""
 
-    $InstallArgs = ""
-
-    Start-Process -FilePath $Exe.FullName -ArgumentList $InstallArgs -NoNewWindow -Wait
+    Start-Process -FilePath $Exe.FullName -ArgumentList $ExeArgs -NoNewWindow -Wait
 }
 
-Function Uninstall-Application {
-    $Exe           = Get-ChildItem -Path "" -Filter ""
-    $UninstallArgs = ""
+function Uninstall-Application {
+    $Exe     = Get-ChildItem -Path '' -Filter '*.exe'
+    $ExeArgs = ""
 
-    Start-Process -FilePath $Exe.FullName -ArgumentList $UninstallArgs -NoNewWindow -Wait -ErrorAction SilentlyContinue
+    if (-not (Test-Path -Path $Exe.FullName)) {
+        break
+    }
+
+    Start-Process -FilePath $Exe.FullName -ArgumentList $ExeArgs -NoNewWindow -Wait
 }
 
-Switch ($Action) {
-    'Install'   { Install-Application -Exe $Exe }
+switch ($Action) {
+    'Install'   { Install-Application }
     'Uninstall' { Uninstall-Application }
 }
